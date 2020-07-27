103349295b96796411d5e22e26889cafbc3eb71e
Evgeny Zhivitsa <evgeny.zhivitsa@gmail.com>
2020-07-27 23:51:00 +0300

    [+] Add main microfrontend service

---
+++ src/services/main/.postcssrc.js
@@ -0,0 +1,22 @@
+const postcssImport = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssNested = require('postcss-nested');
+
+const cssnano = require('cssnano');
+
+module.exports = () => ({
+  plugins: [
+    postcssImport({
+      path: ['src/client/styles']
+    }),
+    postcssNested,
+    postcssPresetEnv({
+      stage: 2,
+      feature: {
+        'nesting-rules': true,
+        'custom-media-queries': true
+      }
+    }),
+    cssnano()
+  ]
+});
